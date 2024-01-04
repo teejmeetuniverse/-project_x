028b363f420072378e18bad6da98f896f96ea224
teejmeetuniverse <teejhdotson@gmail.com>
2024-01-03 22:01:50 -0500

    add uncommitted test

--- tests/test_login.py
+++ tests/test_login.py
@@ -11,25 +11,46 @@ def test_authenticate_with_twitter(httpserver_oauth2):
     """
     response_data = '{"access_token": "abc123"}'
     status_code = 200
-    httpserver_oauth2(response_data=response_data, status_code=status_code)
+    httpserver_oauth2(response_data=response_data, 
+                      status_code=status_code)
     response = authenticate_with_twitter("user", "pass")
     assert response == "abc123"
 
 
 ## create a test where we don't receive a 200 status code
+def test_authenticate_with_twitter(httpserver_oauth2):
 
-    response_data = '{"access_token": "abc123"}'
+    response_data = '{"access_token": "abc456"}'
     # i just changed the status code to a 400 and kept the rest of the code the same
     status_code = 400
-    httpserver_oauth2(response_data=response_data, status_code=status_code)
+    httpserver_oauth2(response_data=response_data, 
+                      status_code=status_code)
     response = authenticate_with_twitter("user", "pass")
-    assert response == "abc123"
+    assert response == "abc456"
+    
 
 ## create a test were we receive a 200 status code but there isn't a access token in the response
+def test_authenticate_with_twitter(httpserver_oauth2):
 
-    response_data = '{"access_token": "abc123"}'
+    response_data = '{"status_code": "200"}'
     status_code = 200
-    httpserver_oauth2(response_data=response_data, status_code=status_code)
+    httpserver_oauth2(response_data=response_data, 
+                      status_code=status_code)
     response = authenticate_with_twitter("user", "pass")
-    # since we are looking for a 200 status code instead of a response i figured we'd change line 35 to the below
-    assert status_code == "200"
+    assert response == 200
+
+
+## asserting an exception when no arguments are passed
+def test_authenicate_with_twitter_exception():
+    
+    with pytest.raises(Exception):
+        authenticate_with_twitter()
+        
+
+## asserting an exception when one arguments is passed
+def test_authenicate_with_twitter_exception_2():
+
+    with pytest.raises(Exception):
+        authenticate_with_twitter("user")
+
+        
